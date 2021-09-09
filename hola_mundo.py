89591a76f7c04ddc052622c3770bf9841a1b35b7
suadler <90269652+suadler@users.noreply.github.com>
2021-09-09 11:21:19 -0300

    Add files via upload

---
+++ hola_mundo.py
@@ -0,0 +1,12 @@
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Inove Coding School
+# Version: 2.0
+
+# Descripcion:
+# Programa creado para ensayar el correcto funcionamiento
+# del entorno de instalación Python
+
+
+print("Hola Mundo!")
+print("Inove tutorial")
